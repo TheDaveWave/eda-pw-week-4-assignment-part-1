d1e3f9dfb2d40e2f7158525a89098ff6bfd2cb0d
David Lindberg <david.wesley.lindberg@gmail.com>
2022-08-08 12:20:34 -0500

    fixed conditional

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -67,17 +67,19 @@ console.log('getLast - should return 6', getLast([2, 3, 6]));
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
 function find( value, array ){
-  for (part of array) {
+  for (let part of array) {
+    //console.log(part);
     if (part === value) {
       return true;
-    } else {
+    } /* else {
       return false;
-    }
+    } */
   }
+  return false;
 }
 
-console.log('find - should return false:', find(6, [1 , 2 ,3]));
-console.log('find - should return true:', find(2, [1 , 2 ,3]));
+console.log('find - should return false:', find(6, [1,2,3]));
+console.log('find - should return true:', find(2, [1,2,3]));
 
 // ----------------------
 // Stretch Goals
