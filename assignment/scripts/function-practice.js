85d97bef0346106b8592424db8cabc9578e15d05
David Lindberg <david.wesley.lindberg@gmail.com>
2022-08-08 11:50:06 -0500

    create an if statement in getLast function

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -53,7 +53,11 @@ console.log( 'isPositive - should say false', isPositive(-3) );
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
 function getLast( array ) {
-
+  if (array === []) {
+    return undefined;
+  } else {
+    return array[array.length - 1];
+  }
 }
 
 // 7. Function to find a value in an array. Return true if the 
